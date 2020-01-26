31b8341908ce57681bc9421fdf521482503e751f
Renato Cantarino <renato.cantarino@gmail.com>
2020-01-25 21:55:00 -0300

    fixes

--- src/components/Avatar/styled.js
+++ src/components/Avatar/styled.js
@@ -6,8 +6,8 @@ import  media from 'styled-media-query'
 export const AvatarHelper = styled(Img)`
 border-radius: 50%;
 height:3.75rem;
-margin auto;
-width:3.75rem
+margin: auto;
+width:3.75rem;
 
 ${media.lessThan("large")`
     height: 1.875rem;
